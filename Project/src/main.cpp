7d68f0a63807cb798ab5f17de6fa9612bd93136d
Olmo <prieto.sanchez.olmo@gmail.com>
2016-08-26 19:13:51 +0200

    Finally fixed problem with gitignore and visual studio files

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
-<<<<<<< HEAD
 
 #include "renderer.h"
-=======
-using namespace std;
->>>>>>> f4bf3d470911fa22689dee0c23d51b7b11b5b5b3
 
 int main() {
-  
-  cout << "hola mundo hola!!" << endl;
 
-<<<<<<< HEAD
   Renderer* renderer = Renderer::getInstance();
   renderer->init();
 
@@ -21,7 +14,5 @@ int main() {
 
   renderer->end();
 
-=======
->>>>>>> f4bf3d470911fa22689dee0c23d51b7b11b5b5b3
   return 0;
 }
